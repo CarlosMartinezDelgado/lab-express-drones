8b8926e5dd9fb7161d3f7f3d498274e7d50feb0c
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-02-21 20:10:27 +0100

    Lab Christian-Carlos sin Css

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  const Drone = require("../models/Drone.model")
+
+  // insertarla en la BD
+  const mongoose = require("mongoose")
+
+  mongoose.connect("mongodb://localhost/lab-express-drones")
+  .then((response) => {
+    console.log("conectados a la BD")
+    
+    return Drone.insertMany(drones) 
+  })
+  .then((response) => {
+    console.log("Drones agregados correctamente")
+
+    // ? hacer la desconexion
+    mongoose.connection.close()
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
